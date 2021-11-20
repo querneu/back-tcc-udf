5d5d889a4144af87ea437276d082a5a91b607ccb
Lucas Soares <lucasoaresleite@gmail.com>
2021-11-20 01:26:26 -0300

    test

--- models/Aluno.model.js
+++ models/Aluno.model.js
@@ -1,36 +1,36 @@
 module.exports = (sequelize, DataTypes) => {
-    const Aluno = sequelize.define("Aluno", {
-      id_aluno: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-      },
-      nome_aluno: {
-        type: DataTypes.STRING,
-      },
-      data_nascimento: {
-        type: DataTypes.STRING,
-      },
-      sexo: {
-        type: DataTypes.STRING,
-      },
-      matricula: {
-        type: DataTypes.STRING,
-      },
-      telefone: {
-        type: DataTypes.STRING,
-      },
-      email: {
-        type: DataTypes.STRING,
-      },
-      is_active:{
-        type:DataTypes.STRING
-      }
-    });
-    Aluno.associate = (models) => {
-      Aluno.belongsTo(models.Turma, {
-        foreignKey: {name:'id_turma'}
-      })
+  const Aluno = sequelize.define("Aluno", {
+    id_aluno: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    nome_aluno: {
+      type: DataTypes.STRING,
+    },
+    data_nascimento: {
+      type: DataTypes.STRING,
+    },
+    sexo: {
+      type: DataTypes.STRING,
+    },
+    matricula: {
+      type: DataTypes.STRING,
+    },
+    telefone: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    is_active: {
+      type: DataTypes.STRING
     }
-    return Aluno;
-  };
+  });
+  Aluno.associate = (models) => {
+    Aluno.hasOne(models.Turma, {
+      foreignKey: { name: 'id_turma' }
+    })
+  }
+  return Aluno;
+};
