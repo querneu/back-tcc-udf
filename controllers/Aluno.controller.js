e8da68769a8b716df60f7052d996771fc920eb64
Lucas Soares <lucasoaresleite@gmail.com>
2021-11-24 16:23:59 -0300

    Changes to aluno controller:  -- Resolving error messages  -- Resolving return messages  -- Resolving delete messages  -- Resolving update messages

--- controllers/Aluno.controller.js
+++ controllers/Aluno.controller.js
@@ -4,8 +4,21 @@ const db = require('../models');
 exports.create = async (req, res) => {
     const data = req.body;
     try {
-        const aluno = await db.Aluno.create(data);
-        res.send(aluno);
+        const aluno = await db.Aluno.create(data).then(
+            res.status(200).JSON({
+                success: true,
+                status: 200,
+                message: "Aluno criado com sucesso",
+                data: aluno
+            })
+        ).catch((err) => {
+            res.send(400).JSON({
+                success: false,
+                status: 400,
+                message: "Erro ao criar Aluno!",
+                info: err
+            })
+        });
     } catch (err) {
         res.send(err);
     }
@@ -13,8 +26,23 @@ exports.create = async (req, res) => {
 
 exports.findAll = async (req, res) => {
     try {
-        const aluno = await db.Aluno.findAll({ include: [{ model: db.Turma, as: 'Turma' }] });
-        res.send(aluno);
+        const aluno = await db.Aluno.findAll(
+            { include: [{ all: true }] }
+        ).then(
+            res.status(200).JSON({
+                success: true,
+                status: 200,
+                message: "Alunos encontrados",
+                data: aluno
+            })
+        ).catch((err) => {
+            res.send(400).JSON({
+                success: false,
+                status: 400,
+                message: "Erro ao listar Alunos!",
+                info: err
+            })
+        });
     } catch (err) {
         res.send(err);
     }
@@ -22,8 +50,24 @@ exports.findAll = async (req, res) => {
 
 exports.findById = async (req, res) => {
     try {
-        const aluno = await db.Aluno.findByPk(req.params.id, { include: [{ model: db.Turma, as: 'Turma' }] });
-        res.send(aluno);
+        const aluno = await db.Aluno.findByPk(req.params.id,
+            {
+                include: [{ all: true }]
+            }).then(
+                res.status(200).JSON({
+                    success: true,
+                    status: 200,
+                    message: "Aluno encontrado",
+                    data: aluno
+                })
+            ).catch((err) => {
+                res.send(400).JSON({
+                    success: false,
+                    status: 400,
+                    message: "Erro ao listar Aluno!",
+                    info: err
+                })
+            });
     } catch (err) {
         res.send(err);
     }
@@ -31,9 +75,24 @@ exports.findById = async (req, res) => {
 
 exports.update = async (req, res) => {
     try {
-        const result = await db.Aluno.update(req.body, { where: { id_aluno: req.params.id } }
-        )
-        res.send(result)
+        const aluno = await db.Aluno.update(
+            req.body,
+            { where: { id_aluno: req.params.id } }
+        ).then(
+            res.status(200).JSON({
+                success: true,
+                status: 200,
+                message: "Aluno criado com sucesso",
+                data: aluno
+            })
+        ).catch((err) => {
+            res.send(400).JSON({
+                success: false,
+                status: 400,
+                message: "Erro ao criar Aluno!",
+                info: err
+            })
+        });
     } catch (err) {
         console.log(err)
         res.send(err)
@@ -42,8 +101,21 @@ exports.update = async (req, res) => {
 
 exports.delete = async (req, res) => {
     try {
-        const result = await db.Aluno.destroy({ where: { id_aluno: req.params.id } });
-        res.status(200).JSON(result);
+        const aluno = await db.Aluno.destroy({ where: { id_aluno: req.params.id } }).then(
+            res.status(200).JSON({
+                success: true,
+                status: 200,
+                message: "Aluno deletado com sucesso",
+                data: aluno
+            })
+        ).catch((err) => {
+            res.send(400).JSON({
+                success: false,
+                status: 400,
+                message: "Erro ao deletar Aluno!",
+                info: err
+            })
+        });
     }
     catch (err) {
         res.send(err)
